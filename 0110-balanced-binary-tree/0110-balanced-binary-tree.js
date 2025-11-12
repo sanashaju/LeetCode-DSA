cb8a3b4e159bb836a9abaf2b47616f136f4809aa
Sana Shaju <sanashajuillikkal@gmail.com>
2025-11-12 21:24:44 +0530

    Time: 2 ms (47.27%), Space: 60.4 MB (51.05%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    function height(node) {
+        if (node === null) return 0;
+
+        const left = height(node.left);
+        const right = height(node.right);
+
+        if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(left, right);
+    }
+
+    return height(root) !== -1;
+};
