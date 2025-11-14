2f6283e1ad01e6174c7e74c2e870b0b58d93db46
Sana Shaju <sanashajuillikkal@gmail.com>
2025-11-14 18:06:40 +0530

    Time: 5 ms (62.96%), Space: 57.3 MB (78.23%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+   const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    const reversed = cleaned.split('').reverse().join('');
+    
+    return cleaned === reversed;   
+};
