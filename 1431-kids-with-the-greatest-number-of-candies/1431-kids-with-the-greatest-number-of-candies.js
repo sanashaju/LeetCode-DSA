37b8df1f05ec8b201f6bb60bd860bf09068511e1
Sana Shaju <sanashajuillikkal@gmail.com>
2025-11-20 23:44:28 +0530

    Time: 0 ms (100%), Space: 56.2 MB (55.25%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const maxCandies = Math.max(...candies);
+
+    return candies.map(candy => candy + extraCandies >= maxCandies);
+};
