0187efe62c67863066cb1fbadf1eb6eef9ac84b4
Sana Shaju <sanashajuillikkal@gmail.com>
2025-11-24 10:16:49 +0530

    Time: 4 ms (16.11%), Space: 62.5 MB (81.46%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while (left < right) {
+       
+        let width = right - left;
+       
+        let h = Math.min(height[left], height[right]);
+
+        let area = h * width;
+
+        max = Math.max(max, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+};
