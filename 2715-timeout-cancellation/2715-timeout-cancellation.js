7071d753d5ce20248068a9160fc27f9140bb5263
Sana Shaju <sanashajuillikkal@gmail.com>
2025-11-25 09:52:24 +0530

    Time: 66 ms (17.88%), Space: 53.8 MB (92.41%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,43 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+
+    // Schedule fn to be executed after t ms
+    const timerId = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+    // Return cancel function → clears the timeout
+    return function cancelFn() {
+        clearTimeout(timerId);
+    };
+};
+
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
