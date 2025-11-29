7f790345d2a42aee1d32b5a3a70f51679c730729
Sana Shaju <sanashajuillikkal@gmail.com>
2025-11-29 09:28:30 +0530

    Time: 13 ms (87.89%), Space: 56.8 MB (72.37%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    
+    let count = 0;
+    
+    for (let i = 0; i < k; i++) {
+        if (vowels.has(s[i])) count++;
+    }
+    
+    let maxCount = count;
+    
+    for (let i = k; i < s.length; i++) {
+        if (vowels.has(s[i])) count++;       
+        if (vowels.has(s[i - k])) count--;   
+        
+        maxCount = Math.max(maxCount, count);
+        
+        if (maxCount === k) return k;
+    }
+    
+    return maxCount;
+};
