66607ad58bac936492333e946d46f4802505ae73
Sana Shaju <sanashajuillikkal@gmail.com>
2025-11-29 09:38:04 +0530

    Time: 3 ms (73.15%), Space: 60.6 MB (73.64%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let left = 0;
+    let zeros = 0;
+    let maxLen = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) zeros++;
+
+        while (zeros > k) {
+            if (nums[left] === 0) zeros--;
+            left++;
+        }
+
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+};
