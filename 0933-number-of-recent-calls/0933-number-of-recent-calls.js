4c036fb11d9a8d9e9bad74aba55c9a3b8972dce3
Sana Shaju <sanashajuillikkal@gmail.com>
2025-12-02 10:23:54 +0530

    Time: 36 ms (46.05%), Space: 70.1 MB (42.64%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.js
@@ -0,0 +1,29 @@
+
+var RecentCounter = function() {
+    
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+var RecentCounter = function() {
+    this.q = [];
+};
+
+RecentCounter.prototype.ping = function(t) {
+    this.q.push(t);
+
+    while (this.q[0] < t - 3000) {
+        this.q.shift();
+    }
+
+    return this.q.length;
+};
+
+
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
