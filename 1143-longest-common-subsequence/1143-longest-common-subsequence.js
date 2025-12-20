bba9272a3b6a2198986c7cdde92a8ce38f6c10eb
Sana Shaju <sanashajuillikkal@gmail.com>
2025-12-20 23:14:22 +0530

    Time: 22 ms (90.6%), Space: 56 MB (88.92%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    const m = text1.length;
+    const n = text2.length;
+
+    let dp = new Array(n + 1).fill(0);
+
+    for (let i = 1; i <= m; i++) {
+        let prev = 0;
+        for (let j = 1; j <= n; j++) {
+            let temp = dp[j];
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[j] = prev + 1;
+            } else {
+                dp[j] = Math.max(dp[j], dp[j - 1]);
+            }
+            prev = temp;
+        }
+    }
+
+    return dp[n];
+};
