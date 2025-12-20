938e1748975cf1c3833a5aea1df1834f6d46963e
Sana Shaju <sanashajuillikkal@gmail.com>
2025-12-20 23:03:08 +0530

    Time: 1 ms (77.49%), Space: 56.1 MB (30.13%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let prev2 = 0;
+    let prev1 = 0;
+
+    for (let i = 2; i <= cost.length; i++) {
+        let curr = Math.min(
+            prev1 + cost[i - 1],
+            prev2 + cost[i - 2]
+        );
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+};
