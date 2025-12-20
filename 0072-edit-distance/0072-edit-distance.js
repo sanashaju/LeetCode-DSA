13ac4eed13dacb03ff1407fa6ceecb37a22182f8
Sana Shaju <sanashajuillikkal@gmail.com>
2025-12-20 23:18:57 +0530

    Time: 4 ms (97.26%), Space: 57.8 MB (68.95%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+    const m = word1.length;
+    const n = word2.length;
+
+    let dp = new Array(n + 1);
+    for (let j = 0; j <= n; j++) dp[j] = j;
+
+    for (let i = 1; i <= m; i++) {
+        let prev = dp[0];
+        dp[0] = i;
+
+        for (let j = 1; j <= n; j++) {
+            let temp = dp[j];
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[j] = prev;
+            } else {
+                dp[j] = 1 + Math.min(
+                    prev,      
+                    dp[j],     
+                    dp[j - 1]  
+                );
+            }
+            prev = temp;
+        }
+    }
+
+    return dp[n];
+};
