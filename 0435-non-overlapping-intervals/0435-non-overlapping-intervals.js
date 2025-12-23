1fa89a75747fe1a7e7ce6cfdcbe3719b2c69e52d
Sana Shaju <sanashajuillikkal@gmail.com>
2025-12-23 22:28:57 +0530

    Time: 68 ms (99.36%), Space: 86.8 MB (66.54%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+    if (intervals.length === 0) return 0;
+
+    intervals.sort((a, b) => a[1] - b[1]);
+
+    let removals = 0;
+    let lastEnd = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] < lastEnd) {
+            removals++;
+        } else {
+            lastEnd = intervals[i][1];
+        }
+    }
+
+    return removals;
+};
