31ceb20315eb65e846b617a6e64a520bc99fc27a
Sana Shaju <sanashajuillikkal@gmail.com>
2025-12-28 23:34:39 +0530

    Time: 50 ms (40.95%), Space: 72.6 MB (59.89%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.js
@@ -0,0 +1,21 @@
+var StockSpanner = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function(price) {
+    let span = 1;
+
+    while (
+        this.stack.length > 0 &&
+        this.stack[this.stack.length - 1][0] <= price
+    ) {
+        span += this.stack.pop()[1];
+    }
+
+    this.stack.push([price, span]);
+    return span;
+};
