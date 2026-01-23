e60a77cc88d5c422af854d6c9535bb2613cfbbc4
Sana Shaju <sanashajuillikkal@gmail.com>
2026-01-23 10:07:27 +0530

    Time: 5 ms (80.13%), Space: 54.9 MB (69.74%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const mapS = {};
+    const mapT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (mapS[s[i]] !== mapT[t[i]]) return false;
+        mapS[s[i]] = i;
+        mapT[t[i]] = i;
+    }
+    return true;
+};
