1cd1592ac59361b5a692cc3d53f6f70125c403ba
Sana Shaju <sanashajuillikkal@gmail.com>
2026-01-24 11:36:57 +0530

    Time: 19 ms (34.17%), Space: 60.9 MB (14.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    const count = new Map();
+
+    for (const ch of s) {
+        count.set(ch, (count.get(ch) || 0) + 1);
+    }
+
+    for (const ch of t) {
+        if (!count.has(ch)) return false;
+        count.set(ch, count.get(ch) - 1);
+        if (count.get(ch) === 0) count.delete(ch);
+    }
+
+    return count.size === 0;
+};
