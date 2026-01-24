0d3ff8b838db4d1b9cf9f5f26594fd9690a98f35
Sana Shaju <sanashajuillikkal@gmail.com>
2026-01-24 11:38:20 +0530

    Time: 38 ms (37.8%), Space: 66.7 MB (85.53%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+
+    for (const word of strs) {
+        const key = word.split("").sort().join("");
+
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(word);
+    }
+
+    return Array.from(map.values());
+};
