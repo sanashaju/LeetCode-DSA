e28413965f319c56130cc65f7ee37dbb94b3912a
Sana Shaju <sanashajuillikkal@gmail.com>
2026-01-30 10:03:50 +0530

    Time: 0 ms (100%), Space: 54.1 MB (28.82%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    const result = [];
+    let i = 0;
+
+    while (i < nums.length) {
+        let start = nums[i];
+
+        while (i + 1 < nums.length && nums[i + 1] === nums[i] + 1) {
+            i++;
+        }
+
+        let end = nums[i];
+
+        if (start === end) {
+            result.push(`${start}`);
+        } else {
+            result.push(`${start}->${end}`);
+        }
+
+        i++;
+    }
+
+    return result;
+};
