873e772c3fe0d52a153a3d092811f1dd169cc1b0
Sana Shaju <sanashajuillikkal@gmail.com>
2026-01-30 10:08:40 +0530

    Time: 7 ms (84.51%), Space: 63.8 MB (89.51%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intervals.length === 0) return [];
+
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const result = [];
+    result.push(intervals[0]);
+
+    for (let i = 1; i < intervals.length; i++) {
+        const last = result[result.length - 1];
+        const current = intervals[i];
+
+        if (current[0] <= last[1]) {
+            last[1] = Math.max(last[1], current[1]);
+        } else {
+            result.push(current);
+        }
+    }
+
+    return result;
+};
