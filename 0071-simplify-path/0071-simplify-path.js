d052ee4e7d41ae32445a19d90f1144c69dca0569
Sana Shaju <sanashajuillikkal@gmail.com>
2026-01-31 09:57:02 +0530

    Time: 2 ms (70.02%), Space: 56.3 MB (56.16%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    const stack = [];
+    const parts = path.split("/");
+
+    for (let part of parts) {
+        if (part === "" || part === ".") {
+            continue; 
+        } else if (part === "..") {
+            if (stack.length > 0) {
+                stack.pop(); 
+            }
+        } else {
+            stack.push(part); 
+        }
+    }
+
+    return "/" + stack.join("/");
+};
