cbccfa4f48664460734d8794242f80f366d407aa
Sana Shaju <sanashajuillikkal@gmail.com>
2026-02-01 23:09:38 +0530

    Time: 9 ms (63.92%), Space: 63.3 MB (99.03%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,40 @@
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+
+    if (this.minStack.length === 0) {
+        this.minStack.push(val);
+    } else {
+        this.minStack.push(Math.min(val, this.getMin()));
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+    this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
